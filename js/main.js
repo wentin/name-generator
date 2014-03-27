6563c066b9e9ebc710d84e889961262e60c7484f
wentin <zhangwenting111@gmail.com>
2014-03-27 15:56:34 -0400

    finalize

--- js/main.js
+++ js/main.js
@@ -1,17 +1,34 @@
 $(function(){
-	$('.keys span').click(function (e) {
-	 	$('.keys span').removeClass('keyDown');
-		var typedText = $('i', this).text().toLowerCase();
-		$('.paper input').val($('.paper input').val() + typedText);
-	})
-	/*$(".input .paper h3").typed({
-        strings: ["Create Your Own Victorian Pen Name"],
-        typeSpeed: 30
-      });*/
+	// $('.keys span').click(function (e) {
+	//  	$('.keys span').removeClass('keyDown');
+	// 	var typedText = $('i', this).text().toLowerCase();
+	// 	$('.paper input').val($('.paper input').val() + typedText);
+	// })
 	$( "input" ).keydown(function(e) {
 	 	var keyValue = String.fromCharCode(e.which);
 	 	console.log(keyValue);
 	 	$('.keys span').removeClass('keyDown');
 	 	$('.keys span:contains('+ keyValue +')').addClass('keyDown');
+	});
+
+	// $('.paper a').click(function(e){
+	// 	e.preventDefault();
+	// 	$('.input.view').removeClass('active');
+	// 	$('.outcome.view').addClass('active');
+	// })
+	var $this;
+	var listenerOn = false;
+	$( "input" ).focus(function(e) {
+		e.preventDefault();
+		$this = $(this);
+		if (!listenerOn) {
+		$('.keys span').click(function (e) {
+		 	$('.keys span').removeClass('keyDown');
+			var typedText = $('i', this).text().toLowerCase();
+			$this.val($this.val() + typedText);
+		})
+		listenerOn = true;
+
+		}
 	});
 })
---
+++ js/victorian.js
@@ -0,0 +1,385 @@
+(function($) {
+    $(function() {
+        var prev = 0;
+        
+        var firstNameChoices = [
+           ["Acton","Arnold","Augustus","Aberforth"],
+           ["Bertram", "Benjamin", "Byron", "Burton", "Bartholomew"],
+           ["Cecil", "Charles", "Chester", "Cornelius", "Cyrus", "Clement"],
+           ["Daniel", "Denton", "Dalton", "Dawson"],
+           ["Edmund", "Egbert", "Ernest", "Ellis"],
+           ["Fitzwilliam", "Frederick", "Francis", "Frank"],
+           ["Garfield", "Gordon", "Gerard", "George", "Gilbert"],
+           ["Hugh", "Harry", "Henry", "Herbert"],
+           ["Ignatius", "Isaac", "Ingram", "Isidore"],
+           ["Jeremiah", "Jasper", "John", "Julian", "Jacob"],
+           ["Kenton", "Kingsley", "Kirk"],
+           ["Lionel", "Laurence", "Leland", "Leonard"],
+           ["Matthew", "Maximilian", "Meriwether", "Mortimer"],
+           ["Nicholas", "Norbert", "Neil"],
+           ["Oscar", "Oliver", "Owen", "Oswald"],
+           ["Paul", "Percy", "Phillip","Peter"],
+           ["Quentin", "Quincy"],
+           ["Raymond","Rodney","Roger","Ralph","Richard"],
+           ["Sacheverell","Simon","Sinclair","Stephen"],
+           ["Theodore","Thomas","Terrence"],
+           ["Upton","Ulysses"],
+           ["Victor","Vaughn","Virgil","Vincent"],
+           ["Wesley","Wallace","Wendell","Weldon","William"],
+           ["Xavier","Xander"],
+           ["Yancy","York"],
+           ["Zebediah","Zachary"]
+        ];
+
+        var lastNameChoices = [
+           ["Adams", "Ashdown", "Alcorn", "Ailey"],
+           ["Bell", "Bamford", "Berry", "Beaton", "Black", "Blewett", "Burrows", "Butler"],
+           ["Carroll", "Cockerfell","Collins","Cunningham","Copperfield"],
+           ["Dalton","Darcy","Davidson","Dickens","Dickenson","Drummond"],
+           ["English","Evans","Eliot"],
+           ["Fletcher","Fezziwig","Fred","Faggins"],
+           ["Gaskell","Gibbons","Goodwood","Gilmore","Gargery"],
+           ["Harding","Hardy","House","Holloway","Havisham"],
+           ["Ivers","Irvin","Isherwood","Infelfinger"],
+           ["James","Jefferies","Jaggers","Jarvis","Jorkins"],
+           ["Keefe","Krook","Kedgick","Kenfield"],
+           ["Lawrence","Little","Lockaby","Lansdown"],
+           ["Mitchell","Morrell","Magnus","Marlowe"],
+           ["Nickleby","Northcutt","Nightingale","Nottingham"],
+           ["Ockleberry","Orlick","Oliver"],
+           ["Pockett","Perkins","Peabody","Plympton"],
+           ["Quin","Quick","Quimby"],
+           ["Radfoot","Rathburn","Reeves","Rawlings","Rochester"],
+           ["Sandiford","Samuelson","Scoville","Sedgwick","Somerset"],
+           ["Tarkington","Tulliver","Thistlethwaite","Throgmorton"],
+           ["Upshaw", "Underhill","Umble"],
+           ["Vickers","Vosper","Venable"],
+           ["Webster","Weston","Warburton","Wickham"],
+           ["Exton","Exley"],
+           ["Yeates","Yarbrough","Yearwood"],
+           ["Shillington","Shelburn","Shreeves"]
+        ];
+        
+        var alphabet = ['a','b']
+
+        function alphaPos(char) { return alphabet.indexOf()}
+        
+        var stateReset = function() {
+
+            $('.input.view').addClass('active');
+            $('.outcome.view').removeClass('active');
+
+            $('#firstName, #lastName').val('');
+        };
+
+        var pickName = function() {
+            var firstName = (document.getElementById("firstName").value);
+            var lastName = (document.getElementById("lastName").value);
+            console.log(firstName + " " + lastName);
+            var firstLetter = (firstName.charAt(0)).toLowerCase();
+            var lastLetter = (lastName.substring(0,1)).toLowerCase();
+            
+            window.newFirstName = firstNameChoices[3][1];
+            window.newLastName = lastNameChoices[2][1];
+            if (firstLetter == "a") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[0].length)));
+                newFirstName = firstNameChoices[0][test1];
+            }
+             else if (firstLetter == "b") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[1].length)));
+                newFirstName = firstNameChoices[1][test1];
+            }
+            else if (firstLetter == "c") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[2].length)));
+                newFirstName = firstNameChoices[2][test1];
+            }
+            else if (firstLetter == "d") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[3].length)));
+                newFirstName = firstNameChoices[3][test1];
+            }
+            else if (firstLetter == "e") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[4].length)));
+                newFirstName = firstNameChoices[4][test1];
+            }
+            else if (firstLetter == "f") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[5].length)));
+                newFirstName = firstNameChoices[5][test1];
+            }
+            else if (firstLetter == "g") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[6].length)));
+                newFirstName = firstNameChoices[6][test1];
+            }
+            else if (firstLetter == "h") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[7].length)));
+                newFirstName = firstNameChoices[7][test1];
+            }
+            else if (firstLetter == "i") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[8].length)));
+                newFirstName = firstNameChoices[8][test1];
+            }
+            else if (firstLetter == "j") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[9].length)));
+                newFirstName = firstNameChoices[9][test1];
+            }
+            else if (firstLetter == "k") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[10].length)));
+                newFirstName = firstNameChoices[10][test1];
+            }
+            else if (firstLetter == "l") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[11].length)));
+                newFirstName = firstNameChoices[11][test1];
+            }
+            else if (firstLetter == "m") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[12].length)));
+                newFirstName = firstNameChoices[12][test1];
+            }
+            else if (firstLetter == "n") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[13].length)));
+                newFirstName = firstNameChoices[13][test1];
+            }
+            else if (firstLetter == "o") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[14].length)));
+                newFirstName = firstNameChoices[14][test1];
+            }
+            else if (firstLetter == "p") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[15].length)));
+                newFirstName = firstNameChoices[15][test1];
+            }
+            else if (firstLetter == "q") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[16].length)));
+                newFirstName = firstNameChoices[16][test1];
+            }
+            else if (firstLetter == "r") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[17].length)));
+                newFirstName = firstNameChoices[17][test1];
+            }
+            else if (firstLetter == "s") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[18].length)));
+                newFirstName = firstNameChoices[18][test1];
+            }
+            else if (firstLetter == "t") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[19].length)));
+                newFirstName = firstNameChoices[19][test1];
+            }
+            else if (firstLetter == "u") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[20].length)));
+                newFirstName = firstNameChoices[20][test1];
+            }
+            else if (firstLetter == "v") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[21].length)));
+                newFirstName = firstNameChoices[21][test1];
+            }
+            else if (firstLetter == "w") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[22].length)));
+                newFirstName = firstNameChoices[22][test1];
+            }
+            else if (firstLetter == "x") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[23].length)));
+                newFirstName = firstNameChoices[23][test1];
+            }
+            else if (firstLetter == "y") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[24].length)));
+                newFirstName = firstNameChoices[24][test1];
+            }
+            else if (firstLetter == "z") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[25].length)));
+                newFirstName = firstNameChoices[25][test1];
+            }
+            //if they enter anything besides the normal alphabet, the generated name will be random
+            else {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices.length)));
+                var test3 = (Math.floor(Math.random() * (firstNameChoices[test1].length)));
+                newFirstName = firstNameChoices[test1][test3];
+            }
+
+            //last name cases
+            if (lastLetter == "a") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[0].length)));
+                newLastName = lastNameChoices[0][test2];
+            }
+            else if (lastLetter == "b") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[1].length)));
+                newLastName = lastNameChoices[1][test2];
+            }
+            else if (lastLetter == "c") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[2].length)));
+                newLastName = lastNameChoices[2][test2];
+            }
+            else if (lastLetter == "d") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[3].length)));
+                newLastName = lastNameChoices[3][test2];
+            }
+            else if (lastLetter == "e") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[4].length)));
+                newLastName = lastNameChoices[4][test2];
+            }
+            else if (lastLetter == "f") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[5].length)));
+                newLastName = lastNameChoices[5][test2];
+            }
+            else if (lastLetter == "g") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[6].length)));
+                newLastName = lastNameChoices[6][test2];
+            }
+            else if (lastLetter == "h") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[7].length)));
+                newLastName = lastNameChoices[7][test2];
+            }
+            else if (lastLetter == "i") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[8].length)));
+                newLastName = lastNameChoices[8][test2];
+            }
+            else if (lastLetter == "j") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[9].length)));
+                newLastName = lastNameChoices[9][test2];
+            }
+            else if (lastLetter == "k") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[10].length)));
+                newLastName = lastNameChoices[10][test2];
+            }
+            else if (lastLetter == "l") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[11].length)));
+                newLastName = lastNameChoices[11][test2];
+            }
+            else if (lastLetter == "m") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[12].length)));
+                newLastName = lastNameChoices[12][test2];
+            }
+            else if (lastLetter == "n") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[13].length)));
+                newLastName = lastNameChoices[13][test2];
+            }
+            else if (lastLetter == "o") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[14].length)));
+                newLastName = lastNameChoices[14][test2];
+            }
+            else if (lastLetter == "p") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[15].length)));
+                newLastName = lastNameChoices[15][test2];
+            }
+            else if (lastLetter == "q") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[16].length)));
+                newLastName = lastNameChoices[16][test2];
+            }
+            else if (lastLetter == "r") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[17].length)));
+                newLastName = lastNameChoices[17][test2];
+            }
+            else if (lastLetter == "s") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[18].length)));
+                newLastName = lastNameChoices[18][test2];
+            }
+            else if (lastLetter == "t") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[19].length)));
+                newLastName = lastNameChoices[19][test2];
+            }
+            else if (lastLetter == "u") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[20].length)));
+                newLastName = lastNameChoices[20][test2];
+            }
+            else if (lastLetter == "v") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[21].length)));
+                newLastName = lastNameChoices[21][test2];
+            }
+            else if (lastLetter == "w") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[22].length)));
+                newLastName = lastNameChoices[22][test2];
+            }
+            else if (lastLetter == "x") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[23].length)));
+                newLastName = lastNameChoices[23][test2];
+            }
+            else if (lastLetter == "y") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[24].length)));
+                newLastName = lastNameChoices[24][test2];
+            }
+            else if (lastLetter == "z") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[25].length)));
+                newLastName = lastNameChoices[25][test2];
+            }
+             else {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices.length)));
+                var test4 = (Math.floor(Math.random() * (lastNameChoices[test2].length)));
+                newLastName = lastNameChoices[test2][test4];
+            }
+
+            //if the new name matches the old name, try again
+            if((firstName.toLowerCase() == (newFirstName.toLowerCase())) || (lastName.toLowerCase() == (newLastName.toLowerCase()))) {
+                console.log("picking again");
+                pickName();
+            }
+        };
+
+
+
+        var stateAnswer = function(c) {
+          pickName();
+          $(".name.result").html(window.newFirstName + " <br/>" + window.newLastName);
+
+        };
+        
+        stateReset();
+        
+        $(".reset-button").click(function() {
+            stateReset();
+            return false;
+        });
+        
+        $(".generate-button").click(function() {
+            if( (document.getElementById("firstName").value).length <2 || (document.getElementById("lastName").value).length <2) {
+                alert("Your publishers need inspiration from a real name!");
+                return false;
+            }
+            
+            $('.input.view').removeClass('active');
+            $('.outcome.view').addClass('active');
+            pickName();
+            stateAnswer();
+            return false; 
+        });
+    });
+})(jQuery);
+
+
+
+window.fbShareBieber = function() {
+            FB.ui({
+                method: 'feed',
+                name: "Victorian Pen Name Generator",
+                link: window.location.href,
+
+                description: 'My new name is "' + window.newFirstName + " " + window.newLastName + '" Get your own pseudonym from the publishers!',
+            }, 
+            function(response) {
+                if (response && response.post_id) {
+                    console.log('FB quiz post was published.');
+                }
+            });
+        };
+
+
+window.fbAsyncInit = function() {
+    FB.init({
+        // for testing on local domain
+        // appId      : "592339267522138",
+        // for huffpost.com domain
+        appId      : "1427100424195799",
+        status     : true,
+        xfbml      : true
+    });
+};
+
+(function(d, s, id){var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) {return;}js = d.createElement(s); js.id = id;js.src = "http://connect.facebook.net/en_US/all.js";fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));
+
+    function getText(text) {
+        if (text.length > 49) {
+            return (text.substring(0,47) + '...');
+        } else {
+            return text;
+        }
+    }
+
+    window.twitterShareBieber = function(quiz, text) {
+        var twitterURL = 'https://twitter.com/share?url=http://huff.to/1fprAj6&text=I got: "' + getText(window.newFirstName) + " " + getText(window.newLastName) + '" Get your own &hashtags=VictorianPenName';
+        window.open(twitterURL, 'targetWindow','toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=300,height=300');
+    };
